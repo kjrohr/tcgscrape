2b46657fb42c02734206201f75f2e99efa504076
Rohr <krohr@krohrMBP.fullsail.com>
2019-08-22 14:31:14 -0400

    Changed header to fit my tool hub'

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,4 @@
-<a href="index.php">Home</a>
+<a href="../toolHub/index.php">Tool Hub</a>
+<a href="index.php">Price Scraper</a>
+<a href="../ccg2_subscriptions/index.php">Subscriber Dashboard</a>
 <hr />
